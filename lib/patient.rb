ee0981b2e46f7f1b1a187498f511b31ce448f39b
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-19 18:34:33 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,4 +15,7 @@ class Patient
     appointment = Appointment.new(self, doctor, date)
   end 
   
+  def appointments 
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end 
 end 
